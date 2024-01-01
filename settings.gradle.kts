77792e14e09874c0093a427a378e462e5f8495d3
Ishan Shehara <mlisliyanage@gmail.com>
2024-01-01 17:18:55 +0530

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "calculator_app_UI2"
+include(":app")
+ 
