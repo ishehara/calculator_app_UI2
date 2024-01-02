7af4db02cba69d298d5bcf477ece05a4994103e3
Ishan Shehara <mlisliyanage@gmail.com>
2024-01-02 21:05:31 +0530

    Migrate to view binding

--- app/src/main/java/com/example/calculator_app_ui2/MainActivity.kt
+++ app/src/main/java/com/example/calculator_app_ui2/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.calculator_app_ui2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calculator_app_ui2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding // this method is use for replacement of android-kotlin-extentions
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
+
+
+
     }
 }
